5d99359e194fd8f235a88d7139e9de88140842a6
yozat02 <youssefziat02@gmail.com>
2020-10-19 14:15:33 +0100

    Updates

---
+++ precache-manifest.15d844d9f26d03049f73cb96789e8f72.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b309a53f547e4878f82e507efef7087",
+    "url": "/demoApp-courses-front/index.html"
+  },
+  {
+    "revision": "fbad1a41452a928e3a1c",
+    "url": "/demoApp-courses-front/static/css/main.0be1f5f7.chunk.css"
+  },
+  {
+    "revision": "9ac8d9ad1bd8037574d8",
+    "url": "/demoApp-courses-front/static/js/2.b48f6b97.chunk.js"
+  },
+  {
+    "revision": "5f983ef0d998e4957796cf6a040e4dc4",
+    "url": "/demoApp-courses-front/static/js/2.b48f6b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4d07106883bd98fe57",
+    "url": "/demoApp-courses-front/static/js/3.18c5f4a9.chunk.js"
+  },
+  {
+    "revision": "c7a02049166afda8d246",
+    "url": "/demoApp-courses-front/static/js/4.1d0f3035.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/demoApp-courses-front/static/js/4.1d0f3035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec475d51e018f13fc86",
+    "url": "/demoApp-courses-front/static/js/5.454e85dd.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/demoApp-courses-front/static/js/5.454e85dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbad1a41452a928e3a1c",
+    "url": "/demoApp-courses-front/static/js/main.51009353.chunk.js"
+  },
+  {
+    "revision": "ea616bada8dbf82c0b5c",
+    "url": "/demoApp-courses-front/static/js/runtime-main.33e40d58.js"
+  }
+]);
